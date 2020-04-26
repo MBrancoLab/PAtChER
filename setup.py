c4795872fac5b7cbd9a94bf0e2e8be4c980047ea
Robert Lowe <rlowe@omnigenbiodata.co.uk>
2020-04-26 16:59:30 +0100

    Add in unpair command

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     entry_points={
         "console_scripts": [
             "patcher = PAtChER.patcher:main".format(__pkg_name__),
+            "unpair = PAtChER.unpair:main".format(__pkg_name__),
         ],
     },
     python_requires='>=3.6',
