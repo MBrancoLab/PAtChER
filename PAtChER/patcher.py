97f8d23fa8bca775a35840e513f08c647321fc62
Branco Lab <33059644+MBrancoLab@users.noreply.github.com>
2021-08-10 15:52:58 +0100

    Changes in ligation site recognition

--- PAtChER/patcher.py
+++ PAtChER/patcher.py
@@ -11,7 +11,7 @@ from .multiproc import run
 from .output import SAMBAMWriter
 
 
-def runsingle(reffile, reads1, reads2, fname, distance, cut_site, min_len, output_type):
+def runsingle(reffile, reads1, reads2, fname, distance, cut_site, min_rec, min_len, output_type):
     """Run single processor version of PAtChER"""
     print("Loading in Reference")
     reference = mp.Aligner(reffile, preset="sr")
@@ -23,10 +23,10 @@ def runsingle(reffile, reads1, reads2, fname, distance, cut_site, min_len, outpu
     while True:
         try:
             read1 = Read(reads1.__next__())
-            read1.split_read(cut_site, min_len)
+            read1.split_read(cut_site, min_rec, min_len)
             read1.qual_trim(10, 10)
             read2 = Read(reads2.__next__())
-            read2.split_read(cut_site, min_len)
+            read2.split_read(cut_site, min_rec, min_len)
             read2.qual_trim(10, 10)
             #r1 and r2 could be None at this stage
             if read1.seq and read2.seq:
@@ -47,30 +47,36 @@ def main():
     parser.add_argument('-r1',
                         '--read1',
                         required=True,
-                        help="Set the read sequence for 1 end (Must be FASTQ/Can be gzipped)")
+                        help="Set the read sequence for 1 end (Must be FASTQ; can be gzipped)")
     parser.add_argument('-r2',
                         '--read2',
                         required=True,
-                        help="Set the read sequence for 2 end (Must be FASTQ/Can be gzipped)")
+                        help="Set the read sequence for 2 end (Must be FASTQ; can be gzipped)")
     parser.add_argument('-d',
                         '--distance',
                         type=int,
                         required=False,
                         default=10000,
-                        help="Set distance to map around total is +/- value")
+                        help="Set distance from unique ends to map non-unique end. Defaults to 10kb.")
     parser.add_argument('-D', '--debug', required=False, action='store_true', help="Turn on debug")
     parser.add_argument('-t', '--threads', type=int, required=False, default=1, help="Set the number of threads")
     parser.add_argument('-c',
                         '--cut-site',
                         required=False,
-                        default="GATC",
-                        help="Cut Site. Defaults to GATC. This will cut GATCGATC")
+                        default="GATCGATC",
+                        help="Cut Site. Defaults to GATCGATC, i.e., a DpnII site after digestion, fill-in and ligation")
+    parser.add_argument('-m',
+                        '--min-rec',
+                        type=int,
+                        required=False,
+                        default=5,
+                        help="Minimum length of cut site to recognise when at the 3' end. Defaults to 5")
     parser.add_argument('-l',
                         '--min-len',
                         type=int,
                         required=False,
                         default=40,
-                        help="Minimum length read to keep for mapping. Defaults to 40")
+                        help="Minimum read length to keep for mapping. Defaults to 40")
     parser.add_argument('-b', '--bam', required=False, action='store_true', help="Output results in BAM format")
     args = parser.parse_args()
 
@@ -92,11 +98,11 @@ def main():
     print(f"Writing to: {args.output}")
     if args.threads >= 3:
         run(args.genome, reads1, reads2, args.output,
-                      args.distance, args.threads, args.cut_site, args.min_len, args.bam, args.debug)
+                      args.distance, args.threads, args.cut_site, args.min_rec, args.min_len, args.bam, args.debug)
     else:
         if args.threads > 1:
-            print("Cannot run multithreading with less than 3 threads defaulting to single")
-        runsingle(args.genome, reads1, reads2, args.output, args.distance, args.cut_site, args.min_len, args.bam)
+            print("Cannot run multithreading with less than 3 threads. Defaulting to single")
+        runsingle(args.genome, reads1, reads2, args.output, args.distance, args.cut_site, args.min_rec, args.min_len, args.bam)
     reads1.close()
     reads2.close()
     print("Run complete")
